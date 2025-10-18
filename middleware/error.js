17f58c05cbd96f9644ec8d886ad8b8158cbd4136
odaymare02 <mareoday123@gmail.com>
2025-10-18 12:36:15 +0300

    sds

--- middleware/error.js
+++ middleware/error.js
@@ -2,6 +2,7 @@ const logger = require('../logger');
 
 module.exports=(err,req,res,next)=>{
     logger.error(err.message, { metadata: err });
+      console.error("Error:", err.message);
     const statusCode=err.statusCode||500;
     const message = statusCode === 500 ? "Something failed." : err.message;
     res.status(statusCode).json({ error: message });
